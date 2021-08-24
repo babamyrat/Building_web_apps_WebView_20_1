7d9caa8e74f0508b2b2f65936591bb216392c83f
Admin <tmstudentler@gmail.com>
2021-08-24 13:13:46 +0300

    step 2

--- app/src/main/java/com/example/building_web_apps_webview_20_1/MainActivity.java
+++ app/src/main/java/com/example/building_web_apps_webview_20_1/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.building_web_apps_webview_20_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = findViewById(R.id.webView);
+
+        //WebView myWebView = findViewById(R.id.webView);
+        WebView myWebView = new WebView(this);
+        setContentView(myWebView);
         myWebView.loadUrl("https://www.google.com/");
     }
 }
