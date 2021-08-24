9c928484f2a75634a06808058df58efbbaa1e579
Admin <tmstudentler@gmail.com>
2021-08-24 12:51:06 +0300

    Initial commit

---
+++ app/src/main/java/com/example/building_web_apps_webview_20_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.building_web_apps_webview_20_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
